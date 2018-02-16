c8a402de9b16117caad10cac7a6f8d0123c33c07
Bram Vonk <bram.vonk@crv4all.com>
2018-02-16 14:54:54 +0100

    Create empty spring boot application

---
+++ src/main/java/nl/bramvonk/homeautomation/mechanicalventilation/SpringBootWebApplication.java
@@ -0,0 +1,11 @@
+package nl.bramvonk.homeautomation.mechanicalventilation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
